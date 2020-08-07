e80afa7705cf13c1c6ffd6eccc2ddadfc8a86767
Jason Chang <jchan149@ucr.edu>
2020-08-07 02:14:14 -0700

    Initial Commit

---
+++ MyWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyWeather
+//
+//  Created by Jason Chang on 8/7/20.
+//  Copyright © 2020 Jason Chang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
